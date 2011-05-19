8f6a665b423fab2104348cbdf442e99e9a12a394
Kevin Locke <kevin@kevinlocke.name>
2011-05-18 21:16:02 -0600

    Update copyright statements
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- ultragetopt.c
+++ ultragetopt.c
@@ -5,7 +5,7 @@
  * define ULTRAGETOPT_REPLACE_GETOPT and include "ultragetopt.h" after the
  * vendor-provided headers for getopt() functions.
  *
- * Copyright (c) 2007, Kevin Locke <kevin@kevinlocke.name>
+ * Copyright (C) 2007-2011, Kevin Locke <kevin@kevinlocke.name>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
