70eb20cc42583778cafb25bd71ccc181536a83c0
Kevin Locke <kevin@kevinlocke.name>
2011-05-18 21:06:34 -0600

    Update and remove cruft from autogen.sh
    Print all warnings from each program invoked.
    Conform to the GNITS standard for automake.
    Remove unused calls.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- autogen.sh
+++ autogen.sh
@@ -5,12 +5,7 @@ set -x
 # Remove the auto* files and get fresh copies
 rm -f aclocal.m4 config.* depcomp install-sh missing ltmain.sh
 rm -rf autom4te.cache
-#rm -rf intl
-#gettextize --intl --no-changelog < /dev/null
-#mv configure.ac~ configure.ac
-#mv Makefile.am~ Makefile.am
-aclocal #-I m4
-#autoheader
+aclocal -Wall
 libtoolize --copy
-automake --add-missing --copy
-autoconf
+automake -Wall --gnits --add-missing --copy
+autoconf -Wall
