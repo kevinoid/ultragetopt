eb5d82ab8e345a01155280064396cf4a00b26f0d
Kevin Locke <kevin@kevinlocke.name>
2016-06-29 12:22:11 -0700

    Set AC_CONFIG_MACRO_DIRS([m4])
    As recommended by libtool.  Along with adding the --install option to
    aclocal, which causes the third-party macros (from autoconf-archive) to
    be copied into the m4 directory rather than appending to aclocal.m4.
    
    This should help with comprehensibility and maintainability of the
    build scripts (as much as that is possible with autotools...) and make
    it easier to add custom macros later.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,8 @@ set -x
 
 # Remove the auto* files and get fresh copies
 rm -rf autom4te.cache build-aux
-aclocal -Wall
+mkdir -p m4
+aclocal -Wall --install -I m4
 libtoolize --copy
 automake -Wall --gnits --add-missing --copy
 autoconf -Wall
