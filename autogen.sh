ac18660a8b41e0eebdce467f41499e5493c37166
Kevin Locke <kevin@kevinlocke.name>
2016-06-29 11:46:33 -0700

    Define AC_CONFIG_AUX_DIR([build-aux])
    Put the auxiliary build files into a directory so that they aren't
    cluttering up the project root.
    
    Choose the name build-aux based on popularity and the discussion at
    https://lists.gnu.org/archive/html/bug-gnulib/2005-03/msg00106.html
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,7 @@
 set -x
 
 # Remove the auto* files and get fresh copies
-rm -f aclocal.m4 config.* depcomp install-sh missing ltmain.sh
-rm -rf autom4te.cache
+rm -rf autom4te.cache build-aux
 aclocal -Wall
 libtoolize --copy
 automake -Wall --gnits --add-missing --copy
